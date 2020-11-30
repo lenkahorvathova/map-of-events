09e1a5fbaf3f03c3a82f56b5af70a29a2aaba4cf
Lenka Horvathova <lenka.m.horvathova@gmail.com>
2020-11-30 13:08:18 +0100

    Added deleting from DB when deleting HTML files.

--- bin/utils/delete_html_files.py
+++ bin/utils/delete_html_files.py
@@ -14,6 +14,7 @@ class DeleteHTMLFiles:
 
     def __init__(self):
         self.args = self._parse_arguments()
+        self.connection = utils.create_connection()
 
     @staticmethod
     def _parse_arguments() -> argparse.Namespace:
@@ -28,11 +29,14 @@ class DeleteHTMLFiles:
                             help="don't store anything permanently")
         parser.add_argument('--domain', type=str, default=None,
                             help="delete HTML file of only the specified calendar domain")
+        parser.add_argument('--with-db-rows', action='store_true', default=False,
+                            help="delete also rows in DB that use the deleted files")
         return parser.parse_args()
 
     def run(self) -> None:
         input_domains = self._load_input_domains()
-        self._delete_html_files(input_domains)
+        removed_files = self._delete_html_files(input_domains)
+        self._delete_db_rows(removed_files)
 
     def _load_input_domains(self) -> List[str]:
         print("Loading input domains...")
@@ -41,7 +45,7 @@ class DeleteHTMLFiles:
             return [self.args.domain]
         return [base['domain'] for base in utils.load_base()]
 
-    def _delete_html_files(self, domain_list: List[str]) -> None:
+    def _delete_html_files(self, domain_list: List[str]) -> List[str]:
         print("Deleting files...")
 
         all_removed_files = []
@@ -49,8 +53,11 @@ class DeleteHTMLFiles:
             domain_removed_files = self._delete_html_files_helper(domain)
             all_removed_files.extend(domain_removed_files)
 
-        print(">> {} files{}deleted:".format(len(all_removed_files), " would be " if self.args.dry_run else " "))
+        print(">> {} files{}deleted{}".format(len(all_removed_files), " would be " if self.args.dry_run else " ",
+                                              ':' if len(all_removed_files) > 0 else ''))
         print(*all_removed_files, sep='\n')
+
+        return all_removed_files
 
     def _delete_html_files_helper(self, domain: str) -> List[str]:
         removed_files = []
@@ -72,6 +79,25 @@ class DeleteHTMLFiles:
                     removed_files.append(file_path)
 
         return removed_files
+
+    def _delete_db_rows(self, removed_files: List[str]) -> None:
+        if self.args.dry_run or not self.args.with_db_rows:
+            return
+
+        print("Deleting DB rows...")
+
+        if self.args.type == "calendars":
+            table_name = "calendar"
+        elif self.args.type == "events":
+            table_name = "event_html"
+        else:
+            return
+
+        query = '''
+                    DELETE FROM {} 
+                    WHERE html_file_path IN ({})
+                '''.format(table_name, ', '.join(["{}".format(str(file_path)) for file_path in removed_files]))
+        self.connection.execute(query)
 
 
 if __name__ == '__main__':
