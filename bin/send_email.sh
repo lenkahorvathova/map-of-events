0ef5e01b1720cc488c39103fe88f75ab4d456873
Lenka Horvathova <lenka.m.horvathova@gmail.com>
2020-11-30 11:58:29 +0100

    Changed a sender name for a weekly reminder email.

--- bin/send_email.sh
+++ bin/send_email.sh
@@ -2,7 +2,7 @@ SUCCESS_EMAIL_FILE=/nlp/projekty/event_map/repository/data/tmp/email.txt
 FAILURE_EMAIL_FILE=/nlp/projekty/event_map/repository/resources/email/failure.txt
 
 if [ -f "$SUCCESS_EMAIL_FILE" ]; then
-  mail -s "Weekly reminder from Map of Events." 445291@mail.muni.cz <${SUCCESS_EMAIL_FILE}
+  mail -s "Weekly reminder" -S "from=Map of Events <no-reply>" 445291@mail.muni.cz <${SUCCESS_EMAIL_FILE}
 else
-  mail -s "Failed weekly reminder from Map of Events." 445291@mail.muni.cz <${FAILURE_EMAIL_FILE}
+  mail -s "Failed weekly reminder" -S "from=Map of Events <no-reply>" 445291@mail.muni.cz <${FAILURE_EMAIL_FILE}
 fi
